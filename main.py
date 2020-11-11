2be835e2d04d444a257e953a7b6e3ff43a6998a8
Suveshan Moodley <suveshan.moodley@gmail.com>
2020-11-11 13:34:08 +0200

    Completed code for game and tested.

--- main.py
+++ main.py
@@ -3,5 +3,17 @@
 import random
 
 # Ask the user how many die they wish to roll
+number_of_die = int(input("Enter the number of die you wish to roll: "))
+
 # Display the values of all the die
+total_dice_value = 0
+dice = 0
+while dice != number_of_die:
+    dice_roll = random.randint(1, 6)
+    print(f"Dice {dice+1}: {dice_roll}")
+    total_dice_value = total_dice_value + dice_roll
+    dice += 1
+
 # Display the sum of all the die values
+print("--------------")
+print(f"Total value of rolled die: {total_dice_value}")
